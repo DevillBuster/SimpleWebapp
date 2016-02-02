50bebb5f1c59962fdde23a360e926e62b7e196ac
DevillBusteer <Bcsf12m530@pucit.edu.pk>
2016-02-02 10:34:14 -0800

    firstcommit

---
+++ SimpleWebApp - EAD/Models/Database.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWebApp___EAD.Models
+{
+    public class MyDatabase : DbContext
+    {
+        public DbSet<Register> Register { get; set; }
+    }
+}
---
+++ SimpleWebApp - EAD/Models/Register.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWebApp___EAD.Models
+{
+    public class Register
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name ="Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name="Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name="Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name ="Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Your Password doesn't match.")]
+        public string ConfirmPassword { get; set; }  
+    }
+}
